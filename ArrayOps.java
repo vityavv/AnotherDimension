287653626b6a7b66c1d070e8992ff8d5ccc1fef5
Victor Veytsman <vityavv@gmail.com>
2020-10-22 13:04:45 -0400

    Add largest method and test it

--- ArrayOps.java
+++ ArrayOps.java
@@ -4,4 +4,10 @@ public class ArrayOps {
 		for (int i = 0; i < arr.length; i++) sum += arr[i];
 		return sum;
 	}
+	public static int largest (int[] arr) {
+		int largest = arr[0];
+		for (int i = 1; i < arr.length; i++)
+			if (arr[i] > largest) largest = arr[i];
+		return largest;
+	}
 }
--- Tester.java
+++ Tester.java
@@ -4,5 +4,7 @@ public class Tester {
 		int[] b = {5, 7, -20, 11, 44};
 		System.out.println(ArrayOps.sum(a) == 15);
 		System.out.println(ArrayOps.sum(b) == 47);
+		System.out.println(ArrayOps.largest(a) == 5);
+		System.out.println(ArrayOps.largest(b) == 44);
 	}
 }
